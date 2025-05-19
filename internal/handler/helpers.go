a82a4b5c2db01f63109f303e61c61bfe915bb1dc
landmines <havrylenko.roman@lll.kpi.ua>
2025-05-19 05:36:54 +0300

    feat: added logging in helpers.go and middleware.go

--- internal/handler/helpers.go
+++ internal/handler/helpers.go
@@ -6,21 +6,26 @@ import (
 	"go.uber.org/zap"
 )
 
-func respondError(c *gin.Context, status int, _ string, err error) {
-	entry := pkg.Logger.With(zap.Int("status", status))
+func respondError(c *gin.Context, status int, message string, err error) {
+	entry := pkg.Logger.With(zap.Int("status", status), zap.String("message", message))
 	if err != nil {
 		entry = entry.With(zap.Error(err))
 	}
-	entry.Warn("error")
+	entry.Warn("error occurred")
+
 	c.Status(status)
 }
 
 func respondSuccess(c *gin.Context, status int, payload gin.H) {
+	entry := pkg.Logger.With(zap.Int("status", status))
+	if payload != nil {
+		entry = entry.With(zap.Any("response", payload))
+	}
+	entry.Info("successful response")
 	if payload == nil {
 		c.Status(status)
 		return
 	}
-
 	c.JSON(status, payload)
 }
 
--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -1,26 +1,36 @@
 package middleware
 
 import (
+	"go.uber.org/zap"
 	"net/http"
 	"regexp"
 
 	"github.com/gin-gonic/gin"
+	"github.com/l4ndm1nes/Weather-API-Application/pkg"
 )
 
 var UUIDRegex = regexp.MustCompile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$`)
 
 func abortWithError(c *gin.Context, status int) {
-	c.JSON(status, gin.H{})
+	entry := pkg.Logger.With(zap.Int("status", status))
+	entry.Warn("invalid token format")
+
+	c.Status(status)
 	c.Abort()
 }
 
 func TokenUUIDRequiredMiddleware(paramName string, errMsg string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Param(paramName)
+		entry := pkg.Logger.With(zap.String("token", token))
+
 		if token == "" || !UUIDRegex.MatchString(token) {
+			entry.Warn("invalid or missing token")
 			abortWithError(c, http.StatusBadRequest)
 			return
 		}
+
+		entry.Info("valid token")
 		c.Set(paramName, token)
 		c.Next()
 	}
